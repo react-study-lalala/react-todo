b65038004712d66d0739ec59b0bfb6f6de4435e0
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-27 11:31:22 +0900

    불필요 컴포넌트 제거

--- src/components/InputItem.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-const InputItem = styled.li`
-    margin: 1rem;
-`
-
-export default InputItem
--- src/components/Label.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-const Label = styled.label`
-    display: inline-block;
-    width: 5rem;
-    text-transform: capitalize;
-`
-
-export default Label

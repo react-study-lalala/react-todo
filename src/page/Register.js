ef90d4108001fce6ef0b4611032558a4a2234b0b
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-27 11:29:03 +0900

    회원가입 화면 컴포넌트 교체

--- src/page/Register.js
+++ src/page/Register.js
@@ -2,17 +2,14 @@ import useInput from '../lib/useInput'
 import { useDispatch } from "react-redux"
 import { register } from '../modules/user'
 import { useHistory } from 'react-router-dom'
-import React, { useCallback } from 'react'
-import Input from "../components/Input"
-import Label from "../components/Label"
-import Wrapper from "../components/Wrapper"
-import InputItem from '../components/InputItem'
-import { SquareButton } from "../components/Button"
+import React, { useCallback, useState } from 'react'
+import { Button, Container, IconButton, TextField } from '@material-ui/core'
+import { Visibility, VisibilityOff } from '@material-ui/icons'
 const Register = () => {
     const { value: name, onChange: onChangeName } = useInput('')
     const { value: email, onChange: onChangeEmail } = useInput('')
     const { value: password, onChange: onChangePassword } = useInput('')
-    const { value: age, onChange: onChangeAge } = useInput(20)
+    const [showPassword, setShowPassword] = useState(false)
 
     const dispatch = useDispatch()
     const history = useHistory()
@@ -21,7 +18,7 @@ const Register = () => {
     async function onSubmit(e) {
         e.preventDefault();
         try {
-            onRegister({ name, email, password, age })
+            onRegister({ name, email, password })
             history.push('/')
         } catch (error) {
             console.log(error)
@@ -29,37 +26,31 @@ const Register = () => {
         }
     }
 
-    return <Wrapper>
-        <form onSubmit={onSubmit}>
-            <ul>
-                <InputItem>
-                    <Label htmlFor="name">
-                        name
-                    </Label>
-                    <Input id="name" type="text" onChange={onChangeName} value={name} required />
-                </InputItem>
-                <InputItem>
-                    <Label htmlFor="email">
-                        email
-                    </Label>
-                    <Input id="email" type="email" onChange={onChangeEmail} value={email} required />
-                </InputItem>
-                <InputItem>
-                    <Label htmlFor="password">
-                        password
-                    </Label>
-                    <Input id="password" type="password" minLength="7" onChange={onChangePassword} value={password} required />
-                </InputItem>
-                <InputItem>
-                    <Label htmlFor="age">
-                        age
-                    </Label>
-                    <Input id="age" type="number" onChange={onChangeAge} value={age} required />
-                </InputItem>
-            </ul>
-            <SquareButton type="submit">Register</SquareButton>
+    function onClickShowPassword() {
+        setShowPassword(val => !val)
+    }
+
+    return <Container maxWidth="sm">
+        <form onSubmit={onSubmit} style={{ textAlign: 'center' }}>
+            <TextField label="Email" type="email" onChange={onChangeEmail} value={email} required fullWidth />
+            <TextField label="Name" type="text" onChange={onChangeName} value={name} required fullWidth />
+            <TextField
+                label="Password"
+                type={showPassword ? 'text' : 'password'}
+                minLength="7"
+                onChange={onChangePassword}
+                value={password}
+                required
+                fullWidth
+                InputProps={{
+                    endAdornment: <IconButton type="button" color="primary" onClick={onClickShowPassword}>
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                }}
+            />
+            <Button type="submit">Register</Button>
         </form>
-    </Wrapper>
+    </Container>
 }
 
 export default React.memo(Register)
